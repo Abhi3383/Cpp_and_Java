18b4eb5cda296231aa351f5884800dc1f340b03f
Abhishek <abhishekgupta52811@gmail.com>
2022-10-03 00:08:59 +0530

    Updated

---
+++ JAVA/BASICS/hello.java
@@ -0,0 +1,12 @@
+//class have variables and methods
+
+class hello {
+    public static void main(String args[]) // this is a method and we have to define,jvm needs a starting point : main
+                                           // static means we dont need object to call main
+                                           // public so that it can be accesed from anywhere
+                                           // void beacuse to show that you dont need to to return anything
+                                           // String args[] bcs we have too many parameters and agrs to pass
+    {
+        System.out.println("Hello world");
+    }
+}
