b1b7d2a5960d544d51f41cef5c9b1dabd9c8f1c3
Abhishek Gupta <b211073@skit.ac.in>
2022-10-02 15:29:12 +0530

    Folders uploaded

---
+++ JAVA/hello.java
@@ -0,0 +1,9 @@
+//class have variables and methods
+
+class hello {
+    public static void main(String args[]) // this is a method and we have to define,jvm needs a starting point : main
+                                           // static means we dont need object to call main
+    {
+        System.out.println("Hello world");
+    }
+}
