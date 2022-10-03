1d6763a116d5c1ef59a9b46c21931e07ba1e3cf5
Abhishek <abhishekgupta52811@gmail.com>
2022-10-03 23:01:09 +0530

    updated

--- JAVA/Pattern Programming/P1.java
+++ JAVA/Pattern Programming/P1.java
@@ -1,11 +1,14 @@
 class P1 {
     public static void main(String args[]) {
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
-        System.out.println("*");
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("*");
+            i++;
+        }
+        System.out.println(" ");
+        do {
+            System.out.println("*");
+            i++;
+        } while (i < 0);
     }
 }
