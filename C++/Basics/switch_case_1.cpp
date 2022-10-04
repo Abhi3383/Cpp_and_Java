b268ad8c39748e8379348f227b752ac3d9d68c7c
Abhishek <abhishekgupta52811@gmail.com>
2022-10-04 17:36:46 +0530

    updated

--- C++/Basics/switch_case_1.cpp
+++ C++/Basics/switch_case_1.cpp
@@ -1,2 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main()
+{
+    int x;
+    switch (x)
+    {
+    case 25:
+        cout << "Atul";
+        break;
+    case -45:
+        cout << "suraj";
+        break;
+    case 0:
+        cout << "Bikash";
+        break;
+    case 5:
+        printf("Hardik");
+    default:
+        break;
+    }
+}
