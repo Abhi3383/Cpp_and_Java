613a86db98f2acf5096a188998c91f12db99b658
Abhishek <abhishekgupta52811@gmail.com>
2022-10-04 11:17:25 +0530

    updated

--- JAVA/Pattern Programming/o_letter.java
+++ JAVA/Pattern Programming/o_letter.java
@@ -1,18 +1,15 @@
-public class A_letter_2 {
+public class o_letter {
     public static void main(String args[]) {
-        int n = 5;
+        int n = 8;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j > 0 && j < n - 1 || j == 0 && i > 0 || j == n - 1 && i > 0 || i == (n - 1) / 2) {
+                if (i == 0 && j != 0 && j != n - 1 || i == n - 1 && j != 0 && j != n - 1
+                        || j == 0 && i != 0 && i != n - 1 || j == n - 1 && i != 0 && i != n - 1) {
                     System.out.print("* ");
-                }
-
-                else {
+                } else
                     System.out.print("  ");
-                }
             }
             System.out.println();
         }
-
     }
 }
