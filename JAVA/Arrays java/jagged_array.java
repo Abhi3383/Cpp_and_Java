c12978b6e8d98cf67235111cca8865c2a39ec5f1
Abhishek <abhishekgupta52811@gmail.com>
2022-10-08 08:21:17 +0530

    updated

--- JAVA/Arrays java/jagged_array.java
+++ JAVA/Arrays java/jagged_array.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
-public class array_2_d {
+public class jagged_array {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] arr = new int[3][4];
+        int[][] arr = new int[3][];
+
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+        arr[2] = new int[3];
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.println("Enter marks if class " + (i) + " Student " + (j));
