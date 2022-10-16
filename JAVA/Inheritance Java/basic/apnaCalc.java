a1dc258c957ff2855606a18bb0679fbe9544c72a
Abhishek <abhishekgupta52811@gmail.com>
2022-10-16 12:21:17 +0530

    updated

---
+++ JAVA/Inheritance Java/basic/apnaCalc.java
@@ -0,0 +1,12 @@
+public class apnaCalc {
+
+  public int add(int num1, int num2) {
+    int num3 = num1 + num2;
+    return num3;
+  }
+
+  public int sub(int num1, int num2) {
+    int num3 = num1 - num2;
+    return num3;
+  }
+}
---
+++ JAVA/Inheritance Java/basic/basic.java
@@ -0,0 +1,10 @@
+public class basic {
+
+  public static void main(String args[]) {
+    apnaCalc obj1 = new apnaCalc();
+    int result = obj1.add(2, 4);
+    System.out.println(result);
+    int results = obj1.sub(4, 2);
+    System.out.println(results);
+  }
+}
