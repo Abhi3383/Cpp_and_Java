a10f573f0a8505b65c71424afe12a0085dbe1c92
Abhishek <abhishekgupta52811@gmail.com>
2022-10-23 00:20:29 +0530

    Updated

--- JAVA/Inheritance Java/demo/demo.java
+++ JAVA/Inheritance Java/demo/demo.java
@@ -1,26 +1,27 @@
 
-class X{
-    int i=3;
+class X {
+    int i = 3;
 }
 
-class A extends X{
-    //int i=5;
-    public void show(){
+class A extends X {
+    // int i=5;
+    public void show() {
         System.out.println("in a show");
     }
 }
 
-class B extends A{
-    int i=7;
-    public void printIvalue(){
-        int i=9;
+class B extends A {
+    int i = 7;
+
+    public void printIvalue() {
+        int i = 9;
         System.out.println(i);
         System.out.println(this.i);
         System.out.println(super.i);
     }
 }
 
-public class demo{
+public class demo {
     public static void main(String args[]) {
         B obj = new B();
         obj.show();
