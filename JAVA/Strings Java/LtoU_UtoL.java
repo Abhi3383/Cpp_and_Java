0b6fa678e144e868d129e7e87c9a526184260ecd
Abhishek Gupta <97604021+Abhi3383@users.noreply.github.com>
2023-01-06 21:21:04 +0530

    Delete JAVA directory

--- JAVA/Strings Java/LtoU_UtoL.java
+++
@@ -1,9 +0,0 @@
-public class LtoU_UtoL {
-    public static void main(String args[]) {
-        String s1 = "RajaRamMohanRoy";
-
-        System.out.println(s1.toLowerCase());
-        System.out.println(s1.toUpperCase());
-        System.out.println(s1.charAt(3));
-    }
-}
--- JAVA/Strings Java/typeCasting.java
+++
@@ -1,9 +0,0 @@
-public class typeCasting {
-    public static void main(String argsp[]) {
-
-        char ch = 'a';
-        ch = (char) (ch - 32);
-        System.out.println(ch);
-
-    }
-}
